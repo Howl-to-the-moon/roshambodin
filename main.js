b390015679bac4a7f3ee2c4dd7914b4eb7dfb68a
Howl-to-the-moon <149625575+Howl-to-the-moon@users.noreply.github.com>
2025-10-04 16:30:36 -0400

    Roshambo V1 implement
    Game works, currently with a 5 round loop.
    
    Will work on a V2 just to polish this up
    before submitting. Indents, Comments, and a
    round selector.

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 //console.log my bbg forevah forevah :3
 console.log("Hello World")
 
-let humanScore = 0;
-let computerScore = 0;
+
 
 
 /* works.
@@ -15,16 +14,291 @@ function getComputerChoice() {
    
     let comChoice = Math.floor(Math.random() * 3);
 
-    return comChoice == 0 ? "Rock" : 
-           comChoice == 1 ? "Paper":
-                            "Siccors";
+    return comChoice == 0 ? "ROCK" : 
+           comChoice == 1 ? "PAPER":
+                            "SICCORS";
 }
 
 /* just gets the human choice via prompt*/
 
 function getHumanChoice() {
 
- prompt("Rock, Paper, or Sccissors?");
+ return prompt("Rock, Paper, or Scissors?");
+
 }
 
-console.log(getHumanChoice)
+
+/* calculates score */
+
+function winScore (humanScore, computerScore){
+    if (humanScore > computerScore) {
+    console.log ("Human wins with " + humanScore + " points!")
+}
+
+else if (humanScore < computerScore) {
+    console.log ("Computer wins with " + computerScore + " points!")
+}
+
+else {
+    console.log("Kill eachother NOW.")
+}
+
+}
+
+
+
+
+/* It DOESNT works */
+   /*const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();*/
+
+function playGame()
+{
+
+    function playRound(humanChoice, computerChoice)
+    {
+
+    
+
+    let glub = humanChoice.toUpperCase();
+     console.log(glub);
+
+     if (glub == "ROCK" ) {
+
+        if (computerChoice == "ROCK") {
+            console.log ('Womp Womp');
+            return;
+        }
+
+        else if (computerChoice == "PAPER"){
+            computerScore++;
+            return console.log ("Computer Wins. Paper beats Rock")
+        }
+
+        else {
+            humanScore++;
+            return console.log("Player wins. Rock beats Siccors");
+        }
+
+    };
+
+     if (glub == "PAPER") {
+
+        if (computerChoice == "ROCK") {
+           
+                humanScore++;
+                return console.log("Player wins. Paper beats Rock");
+         }
+    
+        else if (computerChoice == "PAPER"){
+            
+            console.log ('Womp Womp');
+            return;
+
+        }
+
+        else {
+            computerScore++;
+            return console.log ("Computer Wins. Scissors beats Paper")
+
+    }
+
+    }; 
+
+    if (glub == "SCISSORS"){
+
+        if (computerChoice == "ROCK") {
+           
+             computerScore++;
+            return console.log ("Computer Wins. Rock beats Scissors")
+        }
+
+        else if (computerChoice == "PAPER"){
+           
+            humanScore++;
+            return console.log("Player wins. Rock beats Siccors");
+        }
+
+        else {
+         console.log ('Womp Womp');
+            return;
+        }
+
+    }
+
+
+
+
+    }
+
+        let humanScore = 0;
+        let computerScore = 0;
+        
+
+ 
+    for (let i = 0; i < 5 ; i++){
+        playRound (getHumanChoice(), getComputerChoice());
+    }
+
+
+
+
+winScore(humanScore, computerScore);
+
+}
+
+/* .main(){   lol */
+playGame();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+function playGame()
+{
+
+    function playRound(humanChoice, computerChoice)
+    {
+
+    
+
+    let glub = humanChoice.toUpperCase();
+     console.log(glub);
+
+     if (glub == "ROCK" ) {
+
+        if (computerChoice == "ROCK") {
+            console.log ('Womp Womp');
+            return;
+        }
+
+        else if (computerChoice == "PAPER"){
+            computerScore++;
+            return console.log ("Computer Wins. Paper beats Rock")
+        }
+
+        else {
+            humanScore++;
+            return console.log("Player wins. Rock beats Siccors");
+        }
+
+    };
+
+     if (glub == "PAPER") {
+
+        if (computerChoice == "ROCK") {
+           
+                humanScore++;
+                return console.log("Player wins. Paper beats Rock");
+         }
+    
+        else if (computerChoice == "PAPER"){
+            
+            console.log ('Womp Womp');
+            return;
+
+        }
+
+        else {
+            computerScore++;
+            return console.log ("Computer Wins. Scissors beats Paper")
+
+    }
+
+    }; 
+
+    if (glub == "SCISSORS"){
+
+        if (computerChoice == "ROCK") {
+           
+             computerScore++;
+            return console.log ("Computer Wins. Rock beats Scissors")
+        }
+
+        else if (computerChoice == "PAPER"){
+           
+            humanScore++;
+            return console.log("Player wins. Rock beats Siccors");
+        }
+
+        else {
+         console.log ('Womp Womp');
+            return;
+        }
+
+    }
+
+
+
+
+    }
+
+        let humanScore = 0;
+        let computerScore = 0;
+
+
+ 
+
+playRound (humanSelection, computerSelection);
+playRound (humanSelection, computerSelection);
+playRound (humanSelection, computerSelection);
+playRound (humanSelection, computerSelection);
+playRound (humanSelection, computerSelection);
+
+if (humanScore > computerScore) {
+    console.log ("Human wins with " + humanScore + " points!")
+}
+
+else if (humanScore < computerScore) {
+    console.log ("Computer wins with " + computerScore + " points!")
+}
+
+else (
+    console.log("Kill eachother NOW.")
+)
+
+
+}
+
+
+playGame();
+
+
+*/
