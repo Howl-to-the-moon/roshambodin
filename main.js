0dea34290b5ed6ef1a97316a9357663c04214003
Howl-to-the-moon <149625575+Howl-to-the-moon@users.noreply.github.com>
2025-11-04 14:31:03 -0500

    V0.3 implement
    changed stuff around to make a viewable UI and
    make buttons work. bwah.

--- main.js
+++ main.js
@@ -3,12 +3,11 @@ console.log("Hello World")
 
 
 
-/* gets round amount */
 
-function getRoundChoice(){
-    return prompt("How many rounds, big dawg/dawgette/them?");
-}
-
+/* Scores for the winScore() & round choice */
+let humanScore = 0;
+let computerScore = 0;
+       
 
 /* works.
 math random generatres between large decimals, math floor rounds it to the 
@@ -16,154 +15,163 @@ lowest whole number. multiplying randomm by 3 gives us three evenly
 random numbers to choose from and assign the RPS choices.
 */
 
-    function getComputerChoice() {
+
+
+function getComputerChoice() 
+    {
    
      let comChoice = Math.floor(Math.random() * 3);
 
         return comChoice == 0 ? "ROCK" : 
              comChoice == 1 ?  "PAPER" :
                               "SICCORS";
+   
     }
 
 
 
 /* just gets the human choice via prompt*/
 
-    function getHumanChoice() {
+// 11/4, plays a round each time the button is chose. human choice is built in.
 
-        return prompt("Rock, Paper, or Scissors?");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
 
-    }
+        rock.addEventListener("click", (event) => {
+            event.preventDefault();
+            playRound("ROCK", getComputerChoice());
+            
+        });
 
+        paper.addEventListener("click", (event) => {
+            event.preventDefault();
+            playRound("PAPER", getComputerChoice());
+        });
 
-/* calculates score */
-
-function winScore (humanScore, computerScore)
-{
-    if (humanScore > computerScore) 
-    {
-    console.log ("Human wins with " + humanScore + " points!")
-    alert("Human wins with " + humanScore + " points!")
-    }
-
-    else if (humanScore < computerScore) 
-    {
-    console.log ("Computer wins with " + computerScore + " points!")
-    alert ("Computer wins with " + computerScore + " points!")
-    }
-
-    else 
-    {
-    console.log("Kill eachother NOW.")
-    alert("Kill eachother NOW.")
-    }
-
-}
+        scissors.addEventListener("click", (event) => {
+            event.preventDefault();
+             playRound("SCISSORS", getComputerChoice());
+        });
 
 
-
-
-
-/* everything to do wit da game  */
-function playGame()
-{
 
     /* da game :3 */  
     function playRound(humanChoice, computerChoice)
     {
 
+            //secret tools for later (ie updating html)
+        const gameStatus = document.querySelector("#gameStatus");
+        const humanPoints = document.querySelector("#humanPoints");
+        const computerPoints = document.querySelector("#computerPoints");
+
     
-        /* just easier text and makes it so the word works 
-        no matter the case blah blah i crazy now */
-        let glub = humanChoice.toUpperCase();
+        
+        /*                
+        
+                                       RIP GLUB VARIABLE
+                                       10/04/25 - 11/04/25
+                                       "it's glubbin time!!"
+        
+        
+        
+        
+        
+        */
      
 
         /* 
         Rock Path. I'll explain it rq too.
         Basically just a three way comparison each. Increases score and
-        console.logs a round win message. If same, tie and no increase.
+        updates html with a round win message. If same, tie and no increase.
          Since score is global its chill. Else is always the scissor
         subpath cause else is shorter than scissor.
         */
-        if (glub == "ROCK" ) {
+
+        //and the if constantly checks for a win condition cause i'm not thinking big enough on how to impleemnt it better
+
+
+        // Rock Path
+        if (humanChoice == "ROCK" ) {
 
             if (computerChoice == "ROCK") {
-                console.log ('Womp Womp');
+                gameStatus.textContent = "Womp Womp";
                 return;
             }
 
             else if (computerChoice == "PAPER"){
                 computerScore++;
-                return console.log ("Computer Wins. Paper beats Rock")
+                computerPoints.textContent = computerScore;
+                 if (computerScore == 5) {
+        alert("Computer Wins! You suck at this lol!!");
+    }
+                return gameStatus.textContent = "Computer Wins. Paper beats Rock";
             }
 
             else {
                 humanScore++;
-                return console.log("Player wins. Rock beats Siccors");
+                humanPoints.textContent = humanScore;
+                 if (humanScore >= 5) {
+       return alert("Human wins! You can stop playing now.");
+    }
+                 gameStatus.textContent = "Player wins. Rock beats Siccors";
+
             }
 
         }
-        /* Paper path.  */
-        if (glub == "PAPER") {
+        /* Paper Path  */
+        if (humanChoice == "PAPER") {
 
             if (computerChoice == "ROCK") {
                 humanScore++;
-                return console.log("Player wins. Paper beats Rock");
+                humanPoints.textContent = humanScore;
+                if (humanScore >= 5) {
+       return alert("Human wins! You can stop playing now.");
+    }
+                return gameStatus.textContent = "Player wins. Paper beats Rock";
             }
     
             else if (computerChoice == "PAPER"){
-                console.log ('Womp Womp');
+                gameStatus.textContent = "Womp Womp";
                 return;
             }
 
              else {
                 computerScore++;
-                return console.log ("Computer Wins. Scissors beats Paper");
+                computerPoints.textContent = computerScore;
+                 if (computerScore == 5) {
+        alert("Computer Wins! You suck at this lol!!");
+    }
+                return gameStatus.textContent = "Computer Wins. Scissors beats Paper";
             }
 
-        }
-        /* Scissor path */
-        if (glub == "SCISSORS"){
+        }  
+        /* Scissor P+ath */
+        if (humanChoice == "SCISSORS"){
 
             if (computerChoice == "ROCK") {
                 computerScore++;
-                return console.log ("Computer Wins. Rock beats Scissors")
+                computerPoints.textContent = computerScore;
+                 if (computerScore == 5) {
+        alert("Computer Wins! You suck at this lol!!");
+    }
+                return gameStatus.textContent = "Computer Wins. Rock beats Scissors";  
             }
 
             else if (computerChoice == "PAPER"){
                 humanScore++;
-                return console.log("Player wins. Rock beats Siccors");
+                humanPoints.textContent = humanScore;
+                if (humanScore >= 5) {
+       return alert("Human wins! You can stop playing now.");
+    }
+                return gameStatus.textContent = "Player wins. Rock beats Siccors";
             }
 
             else {
-                console.log ('Womp Womp');
+                gameStatus.textContent = "Womp Womp";
                 return;
             }
 
         }
 
     }
-
-    /* Scores for the winScore() & round choice */
-        let humanScore = 0;
-        let computerScore = 0;
-        let roundChoice = getRoundChoice(); /* hi 83 */
-
-
-    /* Calls as many rounds as desired (if confused look at line 150.) */
-    for (let i = 0; i < roundChoice ; i++){
-        playRound (getHumanChoice(), getComputerChoice());
-    }
-
-
-
-    /* Calculates end score */
-    winScore(humanScore, computerScore);
-
-}
-
-
-
-/* .main(){   lol (aka just calls playgame to start it. like a main.*/
-playGame();
-
